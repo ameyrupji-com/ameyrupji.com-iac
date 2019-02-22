2ac0b7cb99b5f6e9e6a7ab1a9d0b7b4afe777aa9
Amey Rupji <ameyrupji@gmail.com>
2019-02-22 16:33:35 -0600

    Fixing failing builds due to s3_web_hosting module

--- terraform/modules/s3_web_hosting/main.tf
+++ terraform/modules/s3_web_hosting/main.tf
@@ -44,7 +44,8 @@ resource "aws_route53_record" "static_website_route53_record" {
   type    = "A"
 
   alias {
-    name    = "${aws_s3_bucket.static_web_hosting_s3_bucket.website_domain}"
-    zone_id = "${aws_s3_bucket.static_web_hosting_s3_bucket.hosted_zone_id}"
+    name                   = "${aws_s3_bucket.static_web_hosting_s3_bucket.website_domain}"
+    zone_id                = "${aws_s3_bucket.static_web_hosting_s3_bucket.hosted_zone_id}"
+    evaluate_target_health = false
   }
 }
