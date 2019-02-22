9f76078a676664571f9b02ca38c1f0af15bd3bd4
Amey Rupji <ameyrupji@gmail.com>
2019-02-22 16:30:47 -0600

    Updating s3_web_hosting module

--- terraform/modules/s3_web_hosting/main.tf
+++ terraform/modules/s3_web_hosting/main.tf
@@ -39,12 +39,12 @@ data "aws_route53_zone" "static_website_route53_zone" {
 }
 
 resource "aws_route53_record" "static_website_route53_record" {
-  zone_id = "${data.aws_route53_zone.static_website_route53_zone.id}"
+  zone_id = "${data.aws_route53_zone.static_website_route53_zone.zone_id}"
   name    = "${var.subdomain}"
   type    = "A"
 
   alias {
-    name    = "${data.aws_s3_bucket.static_web_hosting_s3_bucket.website_domain}"
-    zone_id = "${data.aws_s3_bucket.static_web_hosting_s3_bucket.hosted_zone_id}"
+    name    = "${aws_s3_bucket.static_web_hosting_s3_bucket.website_domain}"
+    zone_id = "${aws_s3_bucket.static_web_hosting_s3_bucket.hosted_zone_id}"
   }
 }
