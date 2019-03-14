a704c039344cfd8bb6a996982386407664c1f9d8
Amey Rupji <ameyrupji@gmail.com>
2019-03-13 20:08:15 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,7 +95,9 @@ resource "aws_iam_role" "lambda_exec_email" {
       "Sid": "AsumeRole",
       "Action": "sts:AssumeRole",
       "Principal": {
-        "Service": "lambda.amazonaws.com"
+        "Service": [
+          "lambda.amazonaws.com",
+          "apigateway.amazonaws.com"
       },
       "Effect": "Allow"
     }
@@ -167,16 +169,29 @@ resource "aws_api_gateway_rest_api" "domain_api_gateway" {
   description = "Api gateway for ${var.domain}"
 }
 
-resource "aws_api_gateway_resource" "email_proxy_gateway_resource" {
+# resource "aws_api_gateway_resource" "proxy" {
+#   rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   parent_id   = "${aws_api_gateway_rest_api.domain_api_gateway.root_resource_id}"
+#   path_part   = "{proxy+}"
+# }
+
+# resource "aws_api_gateway_method" "proxy" {
+#   rest_api_id   = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   resource_id   = "${aws_api_gateway_resource.proxy.id}"
+#   http_method   = "ANY"
+#   authorization = "NONE"
+# }
+
+resource "aws_api_gateway_resource" "email_api_gateway_resource" {
   rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.domain_api_gateway.root_resource_id}"
-  path_part   = "{email+}"
+  path_part   = "email"
 }
 
 resource "aws_api_gateway_method" "email_gateway_method" {
   rest_api_id   = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.email_proxy_gateway_resource.id}"
-  http_method   = "ANY"
+  resource_id   = "${aws_api_gateway_resource.email_api_gateway_resource.id}"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
@@ -192,11 +207,12 @@ resource "aws_api_gateway_integration" "email_lambda_api_gateway_integration" {
 
 resource "aws_api_gateway_deployment" "domain_api_gateway_deployment" {
   depends_on = [
+    "aws_api_gateway_method.email_gateway_method",
     "aws_api_gateway_integration.email_lambda_api_gateway_integration",
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  stage_name  = "prod"
+  stage_name  = ""
 }
 
 resource "aws_lambda_permission" "email_lambda_api_gateway_permission" {
