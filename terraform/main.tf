c8900e7444bfabf15a15ec45fb4c29e941ef6a17
Amey Rupji <ameyrupji@gmail.com>
2019-03-17 20:45:11 -0500

    Lets try this

--- terraform/main.tf
+++ terraform/main.tf
@@ -239,7 +239,7 @@ resource "aws_lambda_permission" "email_lambda_api_gateway_permission" {
   function_name = "${aws_lambda_function.lambda_function_email.arn}"
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_deployment.domain_api_gateway_deployment.execution_arn}/*/${aws_api_gateway_integration.email_lambda_api_gateway_integration.integration_http_method}/${aws_api_gateway_resource.email_api_gateway_resource.path_part}"
+  source_arn = "${aws_api_gateway_rest_api.domain_api_gateway.arn}/*/${aws_api_gateway_method.email_gateway_method.http_method}/${aws_api_gateway_resource.email_api_gateway_resource.path_part}"
 }
 
 # data "aws_route53_zone" "static_website_rout53_zone" {
