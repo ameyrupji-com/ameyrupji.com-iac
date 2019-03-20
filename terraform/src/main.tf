da1f676b251e37d3f99c2b7ea2b88646da490077
Amey Rupji <ameyrupji@gmail.com>
2019-03-20 10:49:20 -0500

    Lets try to get the develop brach going

--- terraform/src/main.tf
+++ terraform/src/main.tf
@@ -42,210 +42,245 @@ module "s3-images-domain" {
   bucket_name = "${var.images-subdomain}.${var.domain}"
 }
 
-# email lambda function
-resource "aws_iam_role" "lambda_exec_email" {
-  name = "${var.name}-email-lambda-role"
+# # email lambda function
+# resource "aws_iam_role" "lambda_exec_email" {
+#   name = "${var.name}-email-lambda-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "LambdaAsumeRole",
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": [
-          "lambda.amazonaws.com",
-          "apigateway.amazonaws.com"
-        ]
-      },
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
 
-resource "aws_lambda_function" "lambda_function_email" {
-  function_name = "${var.name}-email-lambda"
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "LambdaAsumeRole",
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": [
+#           "lambda.amazonaws.com",
+#           "apigateway.amazonaws.com"
+#         ]
+#       },
+#       "Effect": "Allow"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-  s3_bucket = "${var.domain}-assets"
-  s3_key    = "${var.lambda-version}/email-lambda.py.zip"
-  handler   = "email-lambda.lambda_handler"
-  runtime   = "python3.7"
 
-  role = "${aws_iam_role.lambda_exec_email.arn}"
+# resource "aws_lambda_function" "lambda_function_email" {
+#   function_name = "${var.name}-email-lambda"
 
-  source_code_hash = "${base64sha256(file("../../code/lambdas/zipped/email-lambda.py.zip"))}"
-}
 
-resource "aws_cloudwatch_log_group" "email_lambda_cloudwatch_logs" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda_function_email.function_name}"
-  retention_in_days = 14
-}
+#   s3_bucket = "${var.domain}-assets"
+#   s3_key    = "${var.lambda-version}/email-lambda.py.zip"
+#   handler   = "email-lambda.lambda_handler"
+#   runtime   = "python3.7"
 
-resource "aws_iam_policy" "lambda_logging" {
-  name        = "lambda_logging"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
+#   role = "${aws_iam_role.lambda_exec_email.arn}"
 
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = "${aws_iam_role.lambda_exec_email.name}"
-  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
-}
 
-resource "aws_iam_policy" "ses_access" {
-  name        = "ses_send_email_access"
-  path        = "/"
-  description = "IAM policy for sending emails from a lambda"
+#   source_code_hash = "${base64sha256(file("../../code/lambdas/zipped/email-lambda.py.zip"))}"
+# }
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "SESSendEmailAccess",
-      "Effect": "Allow",
-      "Action": "ses:SendEmail",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
 
-resource "aws_iam_role_policy_attachment" "email_lambda_ses_policy" {
-  role       = "${aws_iam_role.lambda_exec_email.name}"
-  policy_arn = "${aws_iam_policy.ses_access.arn}"
-}
+# resource "aws_cloudwatch_log_group" "email_lambda_cloudwatch_logs" {
+#   name              = "/aws/lambda/${aws_lambda_function.lambda_function_email.function_name}"
+#   retention_in_days = 14
+# }
 
-# api to send emails
-resource "aws_api_gateway_rest_api" "domain_api_gateway" {
-  name        = "${var.name}-api-gateway"
-  description = "Api gateway for api.${var.domain}"
-}
 
-resource "aws_api_gateway_resource" "email_api_gateway_resource" {
-  rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  parent_id   = "${aws_api_gateway_rest_api.domain_api_gateway.root_resource_id}"
-  path_part   = "email"
-}
+# resource "aws_iam_policy" "lambda_logging" {
+#   name        = "lambda_logging"
+#   path        = "/"
+#   description = "IAM policy for logging from a lambda"
 
-resource "aws_api_gateway_method" "email_gateway_method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.email_api_gateway_resource.id}"
-  http_method   = "POST"
-  authorization = "NONE"
-}
 
-resource "aws_api_gateway_integration" "email_lambda_api_gateway_integration" {
-  rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  resource_id = "${aws_api_gateway_method.email_gateway_method.resource_id}"
-  http_method = "${aws_api_gateway_method.email_gateway_method.http_method}"
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "logs:CreateLogStream",
+#         "logs:PutLogEvents"
+#       ],
+#       "Resource": "arn:aws:logs:*:*:*",
+#       "Effect": "Allow"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-  integration_http_method = "POST"
-  type                    = "AWS"
-  uri                     = "${aws_lambda_function.lambda_function_email.invoke_arn}"
 
-  request_templates = {
-    "application/json" = ""
-  }
-}
+# resource "aws_iam_role_policy_attachment" "lambda_logs" {
+#   role       = "${aws_iam_role.lambda_exec_email.name}"
+#   policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+# }
 
-resource "aws_api_gateway_method_response" "email_gateway_method_200" {
-  rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  resource_id = "${aws_api_gateway_resource.email_api_gateway_resource.id}"
-  http_method = "${aws_api_gateway_method.email_gateway_method.http_method}"
-  status_code = "200"
-}
 
-resource "aws_api_gateway_integration_response" "email_api_gateway_integration_response" {
-  depends_on = [
-    "aws_api_gateway_integration.email_lambda_api_gateway_integration",
-  ]
+# resource "aws_iam_policy" "ses_access" {
+#   name        = "ses_send_email_access"
+#   path        = "/"
+#   description = "IAM policy for sending emails from a lambda"
 
-  rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  resource_id = "${aws_api_gateway_resource.email_api_gateway_resource.id}"
-  http_method = "${aws_api_gateway_method.email_gateway_method.http_method}"
-  status_code = "${aws_api_gateway_method_response.email_gateway_method_200.status_code}"
 
-  response_templates {
-    "application/json" = ""
-  }
-}
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "SESSendEmailAccess",
+#       "Effect": "Allow",
+#       "Action": "ses:SendEmail",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-resource "aws_api_gateway_deployment" "domain_api_gateway_deployment" {
-  depends_on = [
-    "aws_api_gateway_method.email_gateway_method",
-    "aws_api_gateway_integration.email_lambda_api_gateway_integration",
-    "aws_api_gateway_method_response.email_gateway_method_200",
-    "aws_api_gateway_integration_response.email_api_gateway_integration_response",
-  ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  stage_name  = "prod"
-}
+# resource "aws_iam_role_policy_attachment" "email_lambda_ses_policy" {
+#   role       = "${aws_iam_role.lambda_exec_email.name}"
+#   policy_arn = "${aws_iam_policy.ses_access.arn}"
+# }
 
-data "aws_caller_identity" "current" {}
 
-resource "aws_lambda_permission" "email_lambda_api_gateway_permission" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda_function_email.arn}"
-  principal     = "apigateway.amazonaws.com"
+# # api to send emails
+# resource "aws_api_gateway_rest_api" "domain_api_gateway" {
+#   name        = "${var.name}-api-gateway"
+#   description = "Api gateway for api.${var.domain}"
+# }
 
-  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_deployment.domain_api_gateway_deployment.rest_api_id}/*/POST/${aws_api_gateway_resource.email_api_gateway_resource.path_part}"
-}
 
-data "aws_route53_zone" "static_website_rout53_zone" {
-  name = "${var.domain}."
-}
+# resource "aws_api_gateway_resource" "email_api_gateway_resource" {
+#   rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   parent_id   = "${aws_api_gateway_rest_api.domain_api_gateway.root_resource_id}"
+#   path_part   = "email"
+# }
 
-data "aws_acm_certificate" "domain_certificate" {
-  domain   = "*.${var.domain}"
-  statuses = ["ISSUED"]
-}
 
-resource "aws_api_gateway_domain_name" "api_gateway_domain_name" {
-  domain_name              = "api.${var.domain}"
-  regional_certificate_arn = "${data.aws_acm_certificate.domain_certificate.arn}"
+# resource "aws_api_gateway_method" "email_gateway_method" {
+#   rest_api_id   = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   resource_id   = "${aws_api_gateway_resource.email_api_gateway_resource.id}"
+#   http_method   = "POST"
+#   authorization = "NONE"
+# }
 
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
 
-resource "aws_route53_record" "api_gateway_route53_record" {
-  zone_id = "${data.aws_route53_zone.static_website_rout53_zone.zone_id}"
-  name    = "api"
-  type    = "A"
+# resource "aws_api_gateway_integration" "email_lambda_api_gateway_integration" {
+#   rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   resource_id = "${aws_api_gateway_method.email_gateway_method.resource_id}"
+#   http_method = "${aws_api_gateway_method.email_gateway_method.http_method}"
 
-  alias {
-    name                   = "${aws_api_gateway_domain_name.api_gateway_domain_name.regional_domain_name}"
-    zone_id                = "${aws_api_gateway_domain_name.api_gateway_domain_name.regional_zone_id}"
-    evaluate_target_health = true
-  }
-}
 
-resource "aws_api_gateway_base_path_mapping" "api_gateway_base_path_mapping" {
-  api_id      = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
-  stage_name  = "${aws_api_gateway_deployment.domain_api_gateway_deployment.stage_name}"
-  domain_name = "${aws_api_gateway_domain_name.api_gateway_domain_name.domain_name}"
-}
+#   integration_http_method = "POST"
+#   type                    = "AWS"
+#   uri                     = "${aws_lambda_function.lambda_function_email.invoke_arn}"
+
+
+#   request_templates = {
+#     "application/json" = ""
+#   }
+# }
+
+
+# resource "aws_api_gateway_method_response" "email_gateway_method_200" {
+#   rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   resource_id = "${aws_api_gateway_resource.email_api_gateway_resource.id}"
+#   http_method = "${aws_api_gateway_method.email_gateway_method.http_method}"
+#   status_code = "200"
+# }
+
+
+# resource "aws_api_gateway_integration_response" "email_api_gateway_integration_response" {
+#   depends_on = [
+#     "aws_api_gateway_integration.email_lambda_api_gateway_integration",
+#   ]
+
+
+#   rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   resource_id = "${aws_api_gateway_resource.email_api_gateway_resource.id}"
+#   http_method = "${aws_api_gateway_method.email_gateway_method.http_method}"
+#   status_code = "${aws_api_gateway_method_response.email_gateway_method_200.status_code}"
+
+
+#   response_templates {
+#     "application/json" = ""
+#   }
+# }
+
+
+# resource "aws_api_gateway_deployment" "domain_api_gateway_deployment" {
+#   depends_on = [
+#     "aws_api_gateway_method.email_gateway_method",
+#     "aws_api_gateway_integration.email_lambda_api_gateway_integration",
+#     "aws_api_gateway_method_response.email_gateway_method_200",
+#     "aws_api_gateway_integration_response.email_api_gateway_integration_response",
+#   ]
+
+
+#   rest_api_id = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   stage_name  = "prod"
+# }
+
+
+# data "aws_caller_identity" "current" {}
+
+
+# resource "aws_lambda_permission" "email_lambda_api_gateway_permission" {
+#   statement_id  = "AllowAPIGatewayInvoke"
+#   action        = "lambda:InvokeFunction"
+#   function_name = "${aws_lambda_function.lambda_function_email.arn}"
+#   principal     = "apigateway.amazonaws.com"
+
+
+#   source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_deployment.domain_api_gateway_deployment.rest_api_id}/*/POST/${aws_api_gateway_resource.email_api_gateway_resource.path_part}"
+# }
+
+
+# data "aws_route53_zone" "static_website_rout53_zone" {
+#   name = "${var.domain}."
+# }
+
+
+# data "aws_acm_certificate" "domain_certificate" {
+#   domain   = "*.${var.domain}"
+#   statuses = ["ISSUED"]
+# }
+
+
+# resource "aws_api_gateway_domain_name" "api_gateway_domain_name" {
+#   domain_name              = "api.${var.domain}"
+#   regional_certificate_arn = "${data.aws_acm_certificate.domain_certificate.arn}"
+
+
+#   endpoint_configuration {
+#     types = ["REGIONAL"]
+#   }
+# }
+
+
+# resource "aws_route53_record" "api_gateway_route53_record" {
+#   zone_id = "${data.aws_route53_zone.static_website_rout53_zone.zone_id}"
+#   name    = "api"
+#   type    = "A"
+
+
+#   alias {
+#     name                   = "${aws_api_gateway_domain_name.api_gateway_domain_name.regional_domain_name}"
+#     zone_id                = "${aws_api_gateway_domain_name.api_gateway_domain_name.regional_zone_id}"
+#     evaluate_target_health = true
+#   }
+# }
+
+
+# resource "aws_api_gateway_base_path_mapping" "api_gateway_base_path_mapping" {
+#   api_id      = "${aws_api_gateway_rest_api.domain_api_gateway.id}"
+#   stage_name  = "${aws_api_gateway_deployment.domain_api_gateway_deployment.stage_name}"
+#   domain_name = "${aws_api_gateway_domain_name.api_gateway_domain_name.domain_name}"
+# }
+
