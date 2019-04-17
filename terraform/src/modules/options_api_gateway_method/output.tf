bbb9cf083368f0371dc3f80e9a730ecacab52b12
Amey Rupji <ameyrupji@gmail.com>
2019-04-17 18:59:54 -0500

    Trying to fix build

--- terraform/src/modules/options_api_gateway_method/output.tf
+++
@@ -1,7 +0,0 @@
-output "api-gateway-resource-id" {
-  value = "${aws_api_gateway_resource.api_gateway_resource.id}"
-}
-
-output "aws-api-gateway-resource-path" {
-  value = "${aws_api_gateway_resource.api_gateway_resource.path}"
-}
