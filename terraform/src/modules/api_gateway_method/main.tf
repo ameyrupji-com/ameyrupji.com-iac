8736ad9124a995d76e527ae9aa4f3b38e5fdf4e2
Amey Rupji <ameyrupji@gmail.com>
2019-04-18 17:56:08 -0500

    Lets try if this works

--- terraform/src/modules/api_gateway_method/main.tf
+++ terraform/src/modules/api_gateway_method/main.tf
@@ -10,13 +10,9 @@ resource "aws_api_gateway_integration" "lambda_api_gateway_integration" {
   resource_id = "${aws_api_gateway_method.gateway_method.resource_id}"
   http_method = "${aws_api_gateway_method.gateway_method.http_method}"
 
-  integration_http_method = "${var.http-method}"
-  type                    = "AWS"
-  uri                     = "${var.lambda-function-invoke-arn}"
-
-  request_templates = {
-    "application/json" = ""
-  }
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda-function-arn}/invocations"
 
   depends_on = ["aws_api_gateway_method.gateway_method"]
 }
@@ -53,6 +49,5 @@ resource "aws_lambda_permission" "lambda_api_gateway_permission" {
   action        = "lambda:InvokeFunction"
   function_name = "${var.lambda-function-arn}"
   principal     = "apigateway.amazonaws.com"
-
   source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${var.api-gateway-rest-api-id}/*/${var.http-method}${var.path}"
 }
