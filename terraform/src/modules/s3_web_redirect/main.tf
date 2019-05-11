2480c094ce0c4f5921bcd3306b193cfd46d4ca32
Amey Rupji <ameyrupji@gmail.com>
2019-05-11 16:44:17 -0500

    Simple s3 bucket for data storage

--- terraform/src/modules/s3_web_redirect/main.tf
+++ terraform/src/modules/s3_web_redirect/main.tf
@@ -1,28 +1,8 @@
-# creating redirect requuest bucket
-resource "aws_s3_bucket" "static_web_hosting_s3_bucket" {
+# creating simple bucket for storage of data
+resource "aws_s3_bucket" "storage_s3_bucket" {
   bucket = "${var.bucket_name}"
-
-  website {
-    redirect_all_requests_to = "${var.redirect_bucket_name}"
-  }
 
   tags {
     Name = "${var.bucket_name}"
-  }
-}
-
-data "aws_route53_zone" "static_website_rout53_zone" {
-  name = "${var.domain}."
-}
-
-resource "aws_route53_record" "static_website_route53_record" {
-  zone_id = "${data.aws_route53_zone.static_website_rout53_zone.zone_id}"
-  name    = "${var.subdomain}"
-  type    = "A"
-
-  alias {
-    name                   = "${aws_s3_bucket.static_web_hosting_s3_bucket.website_domain}"
-    zone_id                = "${aws_s3_bucket.static_web_hosting_s3_bucket.hosted_zone_id}"
-    evaluate_target_health = false
   }
 }
