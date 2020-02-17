60fe4a2d7decfb71901c75447c8fe5d278d6cc13
Amey Rupji <ameyrupji@gmail.com>
2020-02-16 22:36:25 -0600

    Update main.tf

--- terraform/src/modules/api_lambda_with_logging/main.tf
+++ terraform/src/modules/api_lambda_with_logging/main.tf
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "lambda_function" {
   source_code_hash = "${filebase64sha256("../../code/lambdas/zipped/${var.lambda-file-name}.py.zip")}"
 
   lifecycle {
-    ignore_changes = ["source_code_hash", "last_modified"]
+    ignore_changes = ["last_modified"]
   }
 }
 
